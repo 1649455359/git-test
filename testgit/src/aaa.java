b0601789380eb94f3da5cb8eb1941ef2713b9dbc
“liang <1812919251@qq.com>
2021-10-17 23:23:12 +0800

    第2次修改

--- testgit/src/aaa.java
+++ testgit/src/aaa.java
@@ -1,5 +1,7 @@
 public class aaa {
     public static void main(String[] args) {
         System.out.println("这是第一次提交");
+        System.out.println("这是第一次修改");
+        System.out.println("这是第2次修改");
     }
 }
